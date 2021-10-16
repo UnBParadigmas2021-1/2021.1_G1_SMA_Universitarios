4a1161da2d19d2d6f2e9038ccb9856dd6873b4cb
Wictorgirardi <wictor.girardi@gmail.com>
2021-10-15 22:18:24 -0300

    config model with new agents vars

--- src/model.py
+++ src/model.py
@@ -2,31 +2,24 @@ from mesa import Model
 from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 
-from agents import Student, College, Freelance
-from schedule import RandomActivationByType
-
-
+from src.agents import Student, College, Freelance
+from src.schedule import RandomActivationByType
 class CollegeStudent(Model):
-    """
-    College-Student baseado no modelo de predacao
-    """
-
-    height = 20
-    width = 20
-
+    # valores iniciais da aplicação
+    height = 30
+    width = 30
+    tipos_estudantes = 5
     initial_student = 100
     initial_college = 20
 
     student_multiply = 0.05
     college_multiply = 0.03
 
-    college_gain = 20
+    college_gain_from_wage = 20
 
     freela = False
     freela_job_deadline = 30
     student_gain_from_wage = 4
-
-    verbose = False  # Print-monitoring
 
     description = (
         "Um simples modelo para simular a relacao entre trabalhos da faculdade e um universitario tentando ganhar dinheiro(predador-presa)."
@@ -37,11 +30,12 @@ class CollegeStudent(Model):
         height=20,
         width=20,
         initial_student=100,
+        tipos_estudantes=5,
         initial_college=20,
         student_multiply=0.05,
         college_multiply=0.03,
         college_gain_from_wage=20,
-        freela=False,
+        freela=True,
         freela_job_deadline=30,
         student_gain_from_wage=4,
     ):
@@ -73,7 +67,8 @@ class CollegeStudent(Model):
             x = self.random.randrange(self.width)
             y = self.random.randrange(self.height)
             money = self.random.randrange(2 * self.student_gain_from_wage)
-            student = Student(self.next_id(), (x, y), self, True, money)
+            student = Student(self.next_id(), (x, y), self, True, money, tipo=i%tipos_estudantes)
+            print(i%tipos_estudantes)
             self.grid.place_agent(student, (x, y))
             self.schedule.add(student)
 
@@ -82,7 +77,11 @@ class CollegeStudent(Model):
             x = self.random.randrange(self.width)
             y = self.random.randrange(self.height)
             money = self.random.randrange(2 * self.college_gain_from_wage)
-            college = College(self.next_id(), (x, y), self, True, money)
+            tipo_alun_matric = []
+            for tipo_estudante in range(tipos_estudantes):
+                if self.random.randrange(2)%2 == 0:
+                    tipo_alun_matric.append(tipo_estudante)
+            college = College(self.next_id(), (x, y), self, True, money,)
             self.grid.place_agent(college, (x, y))
             self.schedule.add(college)
 
@@ -97,7 +96,8 @@ class CollegeStudent(Model):
                 else:
                     countdown = self.random.randrange(self.freela_job_deadline)
 
-                patch = Freelance(self.next_id(), (x, y), self, is_available, countdown)
+                patch = Freelance(self.next_id(), (x, y),
+                                  self, is_available, countdown)
                 self.grid.place_agent(patch, (x, y))
                 self.schedule.add(patch)
 
@@ -106,27 +106,4 @@ class CollegeStudent(Model):
 
     def step(self):
         self.schedule.step()
-        # collect data
         self.datacollector.collect(self)
-        if self.verbose:
-            print(
-                [
-                    self.schedule.time,
-                    self.schedule.get_type_count(College),
-                    self.schedule.get_type_count(Student),
-                ]
-            )
-
-    def run_model(self, step_count=200):
-
-        if self.verbose:
-            print("Initial number college: ", self.schedule.get_type_count(College))
-            print("Initial number student: ", self.schedule.get_type_count(Student))
-
-        for i in range(step_count):
-            self.step()
-
-        if self.verbose:
-            print("")
-            print("Final number college: ", self.schedule.get_type_count(College))
-            print("Final number student: ", self.schedule.get_type_count(Student))
